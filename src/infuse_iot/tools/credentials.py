3784324c1a51ae93b16e68ec40c71b20031a572e
Jordan Yates <jordan@embeint.com>
2024-11-19 22:05:06 +1000

    tools: credentials: dedicated subcommand
    Add a dedicated subcommand for managing credentials. Includes the option
    to load base network keys into the keyring for usage by the device
    database.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ src/infuse_iot/tools/credentials.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+"""Manage Infuse-IoT credentials"""
+
+__author__ = "Jordan Yates"
+__copyright__ = "Copyright 2024, Embeint Inc"
+
+import yaml
+
+from infuse_iot.util.argparse import ValidFile
+from infuse_iot.commands import InfuseCommand
+
+from infuse_iot import credentials
+
+
+class SubCommand(InfuseCommand):
+    NAME = "credentials"
+    HELP = "Manage Infuse-IoT credentials"
+    DESCRIPTION = "Manage Infuse-IoT credentials"
+
+    @classmethod
+    def add_parser(cls, parser):
+        parser.add_argument("--api-key", type=str, help="Set Infuse-IoT API key")
+        parser.add_argument(
+            "--network", type=ValidFile, help="Load network credentials from file"
+        )
+
+    def __init__(self, args):
+        self.args = args
+
+    def run(self):
+        if self.args.api_key is not None:
+            credentials.set_api_key(self.args.api_key)
+        if self.args.network is not None:
+            # Read the file
+            with self.args.network.open("r") as f:
+                content = f.read()
+            # Validate it is valid yaml
+            network_info = yaml.safe_load(content)
+            credentials.save_network(network_info["id"], content)
