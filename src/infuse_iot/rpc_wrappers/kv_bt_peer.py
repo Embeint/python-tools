833c2cb3159064e771aada965af68fdec3e85929
Jordan Yates <jordan@embeint.com>
2024-12-01 14:17:31 +1000

    rpc_wrappers: kv_bt_peer: added
    Add a wrapper for setting the KV_KEY_BLUETOOTH_PEER value.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ src/infuse_iot/rpc_wrappers/kv_bt_peer.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+import ctypes
+import os
+
+import infuse_iot.generated.rpc_definitions as defs
+from infuse_iot.commands import InfuseRpcCommand
+from infuse_iot.util.argparse import BtLeAddress
+from infuse_iot.util.ctypes import bytes_to_uint8
+
+
+class kv_bt_peer(InfuseRpcCommand, defs.kv_write):
+    HELP = "Configure the peer Bluetooth address"
+    DESCRIPTION = "Configure the peer Bluetooth address"
+
+    class request(ctypes.LittleEndianStructure):
+        _fields_ = [
+            ("num", ctypes.c_uint8),
+        ]
+        _pack_ = 1
+
+    class response(InfuseRpcCommand.VariableSizeResponse):
+        base_fields = []
+        var_name = "rc"
+        var_type = ctypes.c_int16
+
+    @staticmethod
+    def kv_store_value_factory(id, value_bytes):
+        class kv_store_value(ctypes.LittleEndianStructure):
+            _fields_ = [
+                ("id", ctypes.c_uint16),
+                ("len", ctypes.c_uint16),
+                ("data", ctypes.c_ubyte * len(value_bytes)),
+            ]
+            _pack_ = 1
+
+        return kv_store_value(id, len(value_bytes), bytes_to_uint8(value_bytes))
+
+    @classmethod
+    def add_parser(cls, parser):
+        addr_group = parser.add_mutually_exclusive_group(required=True)
+        addr_group.add_argument("--public", type=BtLeAddress, help="Public Bluetooth address")
+        addr_group.add_argument("--random", type=BtLeAddress, help="Random Bluetooth address")
+        addr_group.add_argument("--delete", action="store_true", help="Delete peer device")
+
+    def __init__(self, args):
+        if args.public:
+            addr = bytes(BtLeAddress.to_ctype(defs.rpc_enum_bt_le_addr_type.PUBLIC, args.public))
+        elif args.random:
+            addr = bytes(BtLeAddress.to_ctype(defs.rpc_enum_bt_le_addr_type.RANDOM, args.random))
+        elif args.delete:
+            addr = b""
+        else:
+            raise NotImplementedError
+        self.addr = addr
+
+    def request_struct(self):
+        addr_struct = self.kv_store_value_factory(50, self.addr)
+        request_bytes = bytes(addr_struct)
+        return bytes(self.request(1)) + request_bytes
+
+    def handle_response(self, return_code, response):
+        if return_code != 0:
+            print(f"Invalid data buffer ({return_code})")
+            return
+
+        def print_status(name, rc):
+            if rc < 0:
+                print(f"{name} failed to write ({os.strerror(-rc)})")
+            elif rc == 0:
+                print(f"{name} already matched")
+            else:
+                print(f"{name} updated")
+
+        print_status("Peer address", response.rc[0])
--- src/infuse_iot/util/argparse.py
+++ src/infuse_iot/util/argparse.py
@@ -5,6 +5,9 @@ import pathlib
 import re
 from typing import cast
 
+from infuse_iot.generated.rpc_definitions import rpc_enum_bt_le_addr_type, rpc_struct_bt_addr_le
+from infuse_iot.util.ctypes import bytes_to_uint8
+
 
 class ValidFile:
     """Filesystem path that exists"""
@@ -34,5 +37,14 @@ class BtLeAddress:
         return addr
 
     @classmethod
+    def to_ctype(cls, addr_type: rpc_enum_bt_le_addr_type, value: int) -> rpc_struct_bt_addr_le:
+        """Get a ctype representation of the Bluetooth address"""
+        return rpc_struct_bt_addr_le(
+            addr_type,
+            bytes_to_uint8(value.to_bytes(6, "little")),
+        )
+
+    @classmethod
     def integer_value(cls, string) -> int:
+        """Integer value from address string"""
         return cast(int, cls(string))
