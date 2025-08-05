0355116dac75b4a3ad57df13aacd322fe11f96ca
Jordan Yates <jordan@embeint.com>
2025-08-05 12:23:49 +1000

    tools: ota_upgrade: single diff mode
    Add a new operation mode to `ota_upgrade`, which only works for a single
    diff file at a time, but offers significantly improved data throughput
    due to the `file_write_basic` command being run from the gateways local
    flash, rather than the image being sent over the serial port for each
    connection. Not supported for the `native_bt` gateway.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- src/infuse_iot/tools/ota_upgrade.py
+++ src/infuse_iot/tools/ota_upgrade.py
@@ -5,7 +5,9 @@
 __author__ = "Jordan Yates"
 __copyright__ = "Copyright 2024, Embeint Inc"
 
+import argparse
 import binascii
+import sys
 import time
 
 from rich.live import Live
@@ -18,8 +20,9 @@ from rich.status import Status
 from rich.table import Table
 
 from infuse_iot.commands import InfuseCommand
-from infuse_iot.epacket.packet import Auth
-from infuse_iot.generated.rpc_definitions import file_write_basic, rpc_enum_file_action
+from infuse_iot.common import InfuseID
+from infuse_iot.epacket.packet import Auth, HopReceived
+from infuse_iot.generated.rpc_definitions import bt_file_copy_basic, file_write_basic, rpc_enum_file_action
 from infuse_iot.rpc_client import RpcClient
 from infuse_iot.socket_comms import (
     GatewayRequestConnectionRequest,
@@ -27,6 +30,7 @@ from infuse_iot.socket_comms import (
     default_multicast_address,
 )
 from infuse_iot.util.argparse import ValidFile, ValidRelease
+from infuse_iot.zephyr.errno import errno
 
 
 class SubCommand(InfuseCommand):
@@ -39,7 +43,19 @@ class SubCommand(InfuseCommand):
         self._conn_timeout = args.conn_timeout
         self._min_rssi: int | None = args.rssi
         self._explicit_ids: list[int] = []
-        self._release: ValidRelease = args.release
+        if args.release:
+            self._release: ValidRelease = args.release
+            self._single_diff = None
+        elif args.single:
+            # Find the associated release
+            diff_folder = args.single.parent
+            release_folder = diff_folder.parent
+            if diff_folder.name != "diffs":
+                raise argparse.ArgumentTypeError(f"{args.single} is not in a diff folder")
+            self._release = ValidRelease(str(release_folder))
+            self._single_diff = args.single
+        else:
+            raise NotImplementedError("Unknow upgrade type")
         self._app_name = self._release.metadata["application"]["primary"]
         self._app_id = self._release.metadata["application"]["id"]
         self._new_ver = self._release.metadata["application"]["version"]
@@ -72,9 +88,9 @@ class SubCommand(InfuseCommand):
 
     @classmethod
     def add_parser(cls, parser):
-        parser.add_argument(
-            "--release", "-r", type=ValidRelease, required=True, help="Application release to upgrade to"
-        )
+        upgrade_type = parser.add_mutually_exclusive_group(required=True)
+        upgrade_type.add_argument("--release", "-r", type=ValidRelease, help="Application release to upgrade to")
+        upgrade_type.add_argument("--single", type=ValidFile, help="Single diff")
         parser.add_argument("--rssi", type=int, help="Minimum RSSI to attempt upgrade process")
         parser.add_argument("--log", type=str, help="File to write upgrade results to")
         parser.add_argument(
@@ -116,7 +132,73 @@ class SubCommand(InfuseCommand):
             self.task = self.progress.add_task("", total=len(self.patch_file))
         self.progress.update(self.task, completed=offset)
 
+    def gateway_diff_load(self):
+        assert self._single_diff is not None
+        with self._single_diff.open("rb") as f:
+            patch_file = f.read()
+
+        with self._client.connection(InfuseID.GATEWAY, GatewayRequestConnectionRequest.DataType.COMMAND, 10) as _mtu:
+            rpc_client = RpcClient(self._client, _mtu, InfuseID.GATEWAY)
+            params = file_write_basic.request(rpc_enum_file_action.FILE_FOR_COPY, binascii.crc32(patch_file))
+
+            print(f"Writing '{self._single_diff}' to gateway")
+            hdr, _rsp = rpc_client.run_data_send_cmd(
+                file_write_basic.COMMAND_ID,
+                Auth.DEVICE,
+                bytes(params),
+                patch_file,
+                None,
+                file_write_basic.response.from_buffer_copy,
+            )
+            if hdr.return_code != 0:
+                sys.exit(f"Failed to save diff file to gateway (({errno.strerror(-hdr.return_code)}))")
+            print(f"'{self._single_diff}' written to gateway")
+
+    def run_file_upload(self, live: Live, mtu: int, source: HopReceived):
+        self.state_update(live, f"Uploading patch file to {source.infuse_id:016X}")
+        rpc_client = RpcClient(self._client, mtu, source.infuse_id)
+
+        params = file_write_basic.request(rpc_enum_file_action.APP_CPATCH, binascii.crc32(self.patch_file))
+
+        hdr, _rsp = rpc_client.run_data_send_cmd(
+            file_write_basic.COMMAND_ID,
+            Auth.DEVICE,
+            bytes(params),
+            self.patch_file,
+            self.data_progress_cb,
+            file_write_basic.response.from_buffer_copy,
+        )
+
+        if hdr.return_code == 0:
+            self._pending[source.infuse_id] = time.time() + 60
+
+    def run_file_copy(self, live: Live, mtu: int, source: HopReceived):
+        self.state_update(live, f"Copying patch file to {source.infuse_id:016X}")
+        rpc_client = RpcClient(self._client, mtu, InfuseID.GATEWAY)
+
+        params = bt_file_copy_basic.request(
+            source.interface_address.val.to_rpc_struct(),
+            rpc_enum_file_action.APP_CPATCH,
+            0,
+            len(self.patch_file),
+            binascii.crc32(self.patch_file),
+            1,
+            3,
+        )
+
+        hdr, _rsp = rpc_client.run_standard_cmd(
+            bt_file_copy_basic.COMMAND_ID,
+            Auth.DEVICE,
+            bytes(params),
+            bt_file_copy_basic.response.from_buffer_copy,
+        )
+        if hdr.return_code == 0:
+            self._pending[source.infuse_id] = time.time() + 60
+
     def run(self):
+        if self._single_diff:
+            self.gateway_diff_load()
+
         with Live(self.progress_table(), refresh_per_second=4) as live:
             for source, announce in self._client.observe_announce():
                 self.state_update(live, "Scanning")
@@ -169,6 +251,14 @@ class SubCommand(InfuseCommand):
 
                 # Do we have a valid diff?
                 diff_file = self._release.dir / "diffs" / f"{v_str}.bin"
+                if self._single_diff and self._single_diff != diff_file:
+                    # Not the file we've copied to the gateway flash
+                    self._missing_diffs.add(v_str)
+                    self._handled.append(source.infuse_id)
+                    self._no_diff += 1
+                    self.state_update(live, "Scanning")
+                    continue
+
                 if not diff_file.exists():
                     # Is this a single diff from a different application we know about?
                     diff_file = self._release.dir / "diffs" / f"0x{announce.application:08x}" / f"{v_str}.bin"
@@ -193,24 +283,10 @@ class SubCommand(InfuseCommand):
                     with self._client.connection(
                         source.infuse_id, GatewayRequestConnectionRequest.DataType.COMMAND, self._conn_timeout
                     ) as mtu:
-                        self.state_update(live, f"Uploading patch file to {source.infuse_id:016X}")
-                        rpc_client = RpcClient(self._client, mtu, source.infuse_id)
-
-                        params = file_write_basic.request(
-                            rpc_enum_file_action.APP_CPATCH, binascii.crc32(self.patch_file)
-                        )
-
-                        hdr, _rsp = rpc_client.run_data_send_cmd(
-                            file_write_basic.COMMAND_ID,
-                            Auth.DEVICE,
-                            bytes(params),
-                            self.patch_file,
-                            self.data_progress_cb,
-                            file_write_basic.response.from_buffer_copy,
-                        )
-
-                        if hdr.return_code == 0:
-                            self._pending[source.infuse_id] = time.time() + 60
+                        if self._single_diff:
+                            self.run_file_copy(live, mtu, source)
+                        else:
+                            self.run_file_upload(live, mtu, source)
 
                 except ConnectionRefusedError:
                     self.state_update(live, "Scanning")
