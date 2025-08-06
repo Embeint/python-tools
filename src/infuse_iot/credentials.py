e9fdec2d9fc7177b49b98515913f9035a2a90d06
Jordan Yates <jordan@embeint.com>
2025-08-06 21:36:31 +1000

    credentials: store the location of a custom tools folder
    Add the option to store the location of a custom tools folder to the
    infuse-iot credentials.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- src/infuse_iot/credentials.py
+++ src/infuse_iot/credentials.py
@@ -4,14 +4,14 @@ import keyring
 import yaml
 
 
-def set_api_key(api_key: str):
+def set_api_key(api_key: str) -> None:
     """
     Save the Infuse-IoT API key to the keyring module
     """
     keyring.set_password("infuse-iot", "api-key", api_key)
 
 
-def get_api_key():
+def get_api_key() -> str:
     """
     Retrieve the Infuse-IoT API key from the keyring module
     """
@@ -20,8 +20,14 @@ def get_api_key():
         raise FileNotFoundError("API key does not exist in keyring")
     return key
 
+def delete_api_key() -> None:
+    """
+    Delete the Infuse-IoT API key from the keyring module
+    """
+    keyring.delete_password("infuse-iot", "api-key")
 
-def save_network(network_id: int, network_info: str):
+
+def save_network(network_id: int, network_info: str) -> None:
     """
     Save an Infuse-IoT network key to the keyring module
     """
@@ -38,3 +44,24 @@ def load_network(network_id: int):
     if key is None:
         raise FileNotFoundError(f"Network key {network_id:06x} does not exist in keyring")
     return yaml.safe_load(key)
+
+
+def set_custom_tool_path(path: str):
+    """
+    Save the location of custom Infuse-IoT tools on the filesystem
+    """
+    keyring.set_password("infuse-iot", "custom-tools", path)
+
+
+def get_custom_tool_path() -> str | None:
+    """
+    Retrieve the location of custom Infuse-IoT tools on the filesystem
+    """
+    return keyring.get_password("infuse-iot", "custom-tools")
+
+
+def delete_custom_tool_path() -> None:
+    """
+    Delete the location of custom Infuse-IoT tools on the filesystem
+    """
+    return keyring.delete_password("infuse-iot", "custom-tools")
--- src/infuse_iot/tools/credentials.py
+++ src/infuse_iot/tools/credentials.py
@@ -9,7 +9,7 @@ import yaml
 
 from infuse_iot import credentials
 from infuse_iot.commands import InfuseCommand
-from infuse_iot.util.argparse import ValidFile
+from infuse_iot.util.argparse import ValidDir, ValidFile
 
 
 class SubCommand(InfuseCommand):
@@ -22,6 +22,7 @@ class SubCommand(InfuseCommand):
         parser.add_argument("--api-key", type=str, help="Set Infuse-IoT API key")
         parser.add_argument("--api-key-print", action="store_true", help="Print Infuse-IoT API key")
         parser.add_argument("--network", type=ValidFile, help="Load network credentials from file")
+        parser.add_argument("--custom-tools", type=ValidDir, help="Location of custom tools")
 
     def __init__(self, args):
         self.args = args
@@ -30,7 +31,10 @@ class SubCommand(InfuseCommand):
         if self.args.api_key is not None:
             credentials.set_api_key(self.args.api_key)
         if self.args.api_key_print:
-            print(f"API Key: {credentials.get_api_key()}")
+            try:
+                print(f"API Key: {credentials.get_api_key()}")
+            except FileNotFoundError:
+                print("API Key: N/A")
         if self.args.network is not None:
             # Read the file
             with self.args.network.open("r") as f:
@@ -38,3 +42,5 @@ class SubCommand(InfuseCommand):
             # Validate it is valid yaml
             network_info = yaml.safe_load(content)
             credentials.save_network(network_info["id"], content)
+        if self.args.custom_tools:
+            credentials.set_custom_tool_path(str(self.args.custom_tools.absolute()))
