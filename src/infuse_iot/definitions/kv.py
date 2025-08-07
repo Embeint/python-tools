53acc80208c4de979d23f0fde12c3600ffa0e728
Jordan Yates <jordan@embeint.com>
2025-08-07 17:31:06 +1000

    definitions: new submodule to import definitions
    Add a new location to import TDF, RPC and KV definitions from, enabling
    definitions to be imported from different locations without changes to
    end-user tools or scripts.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ src/infuse_iot/definitions/kv.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+
+from infuse_iot.generated.kv_definitions import slots, structs  # noqa: F401
---
+++ src/infuse_iot/definitions/tdf.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+
+from infuse_iot.generated.tdf_definitions import id_type_mapping, readings, structs  # noqa: F401
