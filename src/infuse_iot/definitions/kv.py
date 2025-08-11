d3cbe7be1f7933a9f2d1467157232c38780f0c3d
Jordan Yates <jordan@embeint.com>
2025-08-11 13:59:22 +1000

    definitions: load extension definitions
    Automatically load extension definitions when importing from
    `infuse_iot.definitions.*`.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- src/infuse_iot/definitions/kv.py
+++ src/infuse_iot/definitions/kv.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python3
 
-from infuse_iot.generated.kv_definitions import slots, structs  # noqa: F401
+import infuse_iot.generated.kv_definitions as _kv_base
+from infuse_iot.util.internal import extension_load as _loader
+
+if extension_module := _loader("kv_definitions"):
+
+    class slots(_kv_base.slots, extension_module.slots):  # type: ignore
+        pass
+
+    class structs(_kv_base.structs, extension_module.structs):  # type: ignore
+        pass
+
+else:
+    slots = _kv_base.slots  # type: ignore
+    structs = _kv_base.structs  # type: ignore
+
+__all__ = [
+    "slots",
+    "structs",
+]
