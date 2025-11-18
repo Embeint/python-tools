1439c3715011c44359188da4e3497b738fde6419
Jordan Yates <jordan@embeint.com>
2025-11-18 19:14:29 +1000

    util: crypto: hkdf_derive types
    Add types to the `hkdf_derive` function.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- src/infuse_iot/util/crypto.py
+++ src/infuse_iot/util/crypto.py
@@ -5,7 +5,7 @@ from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 
 
-def hkdf_derive(input_key, salt, info):
+def hkdf_derive(input_key: bytes, salt: bytes, info: bytes) -> bytes:
     """Derive a cryptographic key using HKDF-SHA256"""
     hkdf = HKDF(
         algorithm=hashes.SHA256(),
@@ -16,13 +16,13 @@ def hkdf_derive(input_key, salt, info):
     return hkdf.derive(input_key)
 
 
-def chachapoly_encrypt(key: bytes, associated_data: bytes, nonce: bytes, payload: bytes) -> bytes:
+def chachapoly_encrypt(key: bytes, associated_data: bytes | None, nonce: bytes, payload: bytes) -> bytes:
     """Encrypt a payload using ChaCha20-Poly1305"""
     cipher = ChaCha20Poly1305(key)
     return cipher.encrypt(nonce, payload, associated_data)
 
 
-def chachapoly_decrypt(key: bytes, associated_data: bytes, nonce: bytes, payload: bytes) -> bytes:
+def chachapoly_decrypt(key: bytes, associated_data: bytes | None, nonce: bytes, payload: bytes) -> bytes:
     """Decrypt a payload using ChaCha20-Poly1305"""
     cipher = ChaCha20Poly1305(key)
     return cipher.decrypt(nonce, payload, associated_data)
