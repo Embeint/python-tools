226a8d948b5894b37c74874b79fd15580afaf738
Jordan Yates <jordan@embeint.com>
2026-02-23 14:27:34 +1000

    tools: csv_plot: support grouping files
    Add support for plotting data from multiple files onto a single plot.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- src/infuse_iot/tools/csv_plot.py
+++ src/infuse_iot/tools/csv_plot.py
@@ -25,38 +25,47 @@ class SubCommand(InfuseCommand):
         )
         parser.add_argument("--start", type=str, default="2024-01-01", help="Display data after")
         parser.add_argument("--field", type=str, help="Single column to plot")
+        parser.add_argument("--group", action="store_true", help="Group all lines onto a single plot")
 
     def __init__(self, args):
         self.files = args.files
         self.field = args.field
         self.start = args.start
+        self.group = args.group
 
     def run(self):
         import pandas as pd
         import plotly.express as px
+        import plotly.graph_objects as go
         from dash import Dash, dcc, html
 
-        figures = []
+        fig: None | go.Figure = None
+        figures: list[dcc.Graph] = []
+        if self.group:
+            fig = go.Figure()
+
         for file in self.files:
             df = pd.read_csv(file)
 
             mask = df["time"] >= self.start
             filtered_df = df.loc[mask]
 
-            if self.field:
-                y_data = filtered_df[self.field]
+            y_data = filtered_df[self.field] if self.field else filtered_df.columns.values[1:]
+            if self.group:
+                assert fig is not None
+                fig.add_trace(go.Scatter(x=filtered_df["time"], y=y_data, name=str(file.name), mode="lines"))
             else:
-                y_data = filtered_df.columns.values[1:]
-
-            fig = px.line(
-                filtered_df,
-                x="time",
-                y=y_data,
-                title=str(file),
-            )
-            figures.append(dcc.Graph(figure=fig))
+                fig = px.line(
+                    filtered_df,
+                    x="time",
+                    y=y_data,
+                    title=str(file),
+                )
+                figures.append(dcc.Graph(figure=fig))
 
         app = Dash()
+        if self.group:
+            figures = [dcc.Graph(figure=fig, style={"height": "90vh"})]
         app.layout = html.Div(figures)
 
         app.run(debug=True)
