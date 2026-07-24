7ad7028306b334b72cc0897a5634fbd75583ba47
Jordan Yates <jordan@embeint.com>
2026-07-24 11:17:53 +1000

    api_client: regenerate for new endpoint
    Regenerate client to get access to new auth endpoint.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

---
+++ src/infuse_iot/api_client/models/get_device_shared_secret_body.py
@@ -0,0 +1,85 @@
+from __future__ import annotations
+
+from collections.abc import Mapping
+from typing import TYPE_CHECKING, Any, TypeVar
+
+from attrs import define as _attrs_define
+from attrs import field as _attrs_field
+
+from ..types import UNSET, Unset
+
+if TYPE_CHECKING:
+    from ..models.security_state import SecurityState
+
+
+T = TypeVar("T", bound="GetDeviceSharedSecretBody")
+
+
+@_attrs_define
+class GetDeviceSharedSecretBody:
+    """
+    Attributes:
+        device_id (str): The ID of the device as a hex string Example: d291d4d66bf0a955.
+        security_state (SecurityState | Unset):
+    """
+
+    device_id: str
+    security_state: SecurityState | Unset = UNSET
+    additional_properties: dict[str, Any] = _attrs_field(init=False, factory=dict)
+
+    def to_dict(self) -> dict[str, Any]:
+        device_id = self.device_id
+
+        security_state: dict[str, Any] | Unset = UNSET
+        if not isinstance(self.security_state, Unset):
+            security_state = self.security_state.to_dict()
+
+        field_dict: dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update(
+            {
+                "deviceId": device_id,
+            }
+        )
+        if security_state is not UNSET:
+            field_dict["securityState"] = security_state
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: type[T], src_dict: Mapping[str, Any]) -> T:
+        from ..models.security_state import SecurityState
+
+        d = dict(src_dict)
+        device_id = d.pop("deviceId")
+
+        _security_state = d.pop("securityState", UNSET)
+        security_state: SecurityState | Unset
+        if isinstance(_security_state, Unset):
+            security_state = UNSET
+        else:
+            security_state = SecurityState.from_dict(_security_state)
+
+        get_device_shared_secret_body = cls(
+            device_id=device_id,
+            security_state=security_state,
+        )
+
+        get_device_shared_secret_body.additional_properties = d
+        return get_device_shared_secret_body
+
+    @property
+    def additional_keys(self) -> list[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
