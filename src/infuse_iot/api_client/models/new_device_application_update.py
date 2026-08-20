21a20367396f9a422b1aebb6c362086df539884b
Jordan Yates <jordan@embeint.com>
2026-08-20 16:37:13 +1000

    api_client: regenerate from specification
    Regenerate the API client from the OpenAPI spec as of `20/08/2026`.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- src/infuse_iot/api_client/models/new_device_application_update.py
+++ src/infuse_iot/api_client/models/new_device_application_update.py
@@ -6,6 +6,8 @@ from typing import Any, TypeVar
 from attrs import define as _attrs_define
 from attrs import field as _attrs_field
 
+from ..types import UNSET, Unset
+
 T = TypeVar("T", bound="NewDeviceApplicationUpdate")
 
 
@@ -14,13 +16,18 @@ class NewDeviceApplicationUpdate:
     """
     Attributes:
         release_id (str): ID of application release to update device to
+        max_attempts (int | Unset): Maximum number of attempts to update the device - set to 0 for unlimited attempts
+            Default: 5.
     """
 
     release_id: str
+    max_attempts: int | Unset = 5
     additional_properties: dict[str, Any] = _attrs_field(init=False, factory=dict)
 
     def to_dict(self) -> dict[str, Any]:
         release_id = self.release_id
+
+        max_attempts = self.max_attempts
 
         field_dict: dict[str, Any] = {}
         field_dict.update(self.additional_properties)
@@ -29,6 +36,8 @@ class NewDeviceApplicationUpdate:
                 "releaseId": release_id,
             }
         )
+        if max_attempts is not UNSET:
+            field_dict["maxAttempts"] = max_attempts
 
         return field_dict
 
@@ -37,8 +46,11 @@ class NewDeviceApplicationUpdate:
         d = dict(src_dict)
         release_id = d.pop("releaseId")
 
+        max_attempts = d.pop("maxAttempts", UNSET)
+
         new_device_application_update = cls(
             release_id=release_id,
+            max_attempts=max_attempts,
         )
 
         new_device_application_update.additional_properties = d
